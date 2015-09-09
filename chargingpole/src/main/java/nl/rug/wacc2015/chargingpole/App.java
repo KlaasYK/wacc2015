c7936cc839f7ae7922094a088f3261616d6f53a9
KlaasYK <klaasyk@users.noreply.github.com>
2015-09-09 12:48:10 +0200

    Initial commit of charginpole

---
+++ chargingpole/src/main/java/nl/rug/wacc2015/chargingpole/App.java
@@ -0,0 +1,17 @@
+package nl.rug.wacc2015.chargingpole;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Charging pole entry point
+ * 
+ */
+public class App {
+   
+    public static void main(String[] args) {
+    	Logger l = LoggerFactory.getLogger(App.class);
+    	l.info("{} {}", "Hello","World!");
+    }
+    
+}
