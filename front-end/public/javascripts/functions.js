cf2c35f62ba02a8e762c42124905be691e6275cc
Rick van der Mark <rvdmark@gmail.com>
2015-10-06 22:33:45 +0200

    Front-end grid updates

--- front-end/public/javascripts/functions.js
+++ front-end/public/javascripts/functions.js
@@ -7,35 +7,24 @@ pstatus.IDLE = 1;
 pstatus.CHARGING = 2;
 
 var DOWNTIMEOUT = 25000;
+
+$.jgrid.defaults.styleUI = 'Bootstrap';
+
 function setTimeOutClosure(poleid) {
     return setTimeout(function(){
         poledata[poleid].status = pstatus.DOWN;
-        // TODO: Update the view with this pole down
-        console.log(poleid + " is down!");
+
+        // Update grid
+        var rowData = $('#jqGrid').jqGrid('getRowData', poleid);
+        rowData.status = 0;
+        $('#jqGrid').jqGrid('setRowData', poleid, rowData);
+
+        // TODO: Update notifications
+
+
     },DOWNTIMEOUT);
 }
 
-$.getJSON( "./stations", function( jsondata ) {
-  json = jsondata;
-  console.log("Loaded location data");
-    // structure which prevents looping in heartbeat updates ;)
-    for (var i = 0; i < json.length; i++) {
-        var pole = json[i];
-        poledata[pole.id] = pole;
-        var curtime = new Date().getTime();
-        if (pole.status != pstatus.DOWN && (pole.lastHeartbeat + DOWNTIMEOUT) > curtime) {
-            // Set timeout
-            poledata[pole.id].timeout = setTimeOutClosure(pole.id);
-        } else {
-            poledata[pole.id].status = pstatus.DOWN;
-            console.log(pole.id + " is down!");
-        }
-    }
-    // TODO: update the grid
-});
-
-$.jgrid.defaults.styleUI = 'Bootstrap';
- 
 $(document).ready(function () {
     var socket = new WebSocket('ws://' + location.host + "/stationsfeed");
 
@@ -46,8 +35,15 @@ $(document).ready(function () {
 
     socket.onmessage = function (event) {
         var pole = JSON.parse(event.data);
-        console.log(pole);
-        if (poledata[pole.id].timeout) {clearTimeout(poledata[pole.id].timeout);}
+
+        // Update grid
+        if ( $('#jqGrid').jqGrid('getInd', pole.id) )
+            $('#jqGrid').jqGrid('setRowData', pole.id, pole);
+        else
+            $('#jqGrid').jqGrid('addRowData', pole.id, pole);
+
+        if (poledata[pole.id] && poledata[pole.id].timeout) {clearTimeout(poledata[pole.id].timeout); }
+
         poledata[pole.id] = pole;
         poledata[pole.id].timeout = setTimeOutClosure(pole.id);
     };
@@ -60,28 +56,6 @@ $(document).ready(function () {
     $(window).bind('beforeunload', function(){
         socket.close();
     });
-
-    var grid = $("#jqGrid").jqGrid({
-            url: '/stations',
-            datatype: "json",
-             colModel: [
-                    { label: 'Pole ID', name: 'id', width: 150 },
-                    //{ label: 'Street', name: 'street', width: 90 },
-                    //{ label: 'City', name: 'city', width: 100 },
-                    { label: 'Status', name: 'status', width: 80, sorttype: 'text' },
-                    { label: 'Last active', name:'lastHeartbeat', width: 100, formatter:'date', formatoptions: {srcformat: 'U/1000', newformat:'d/m/Y H:i:s'}}
-            ],
-            autowidth: true,
-            shrinkToFit: true,
-            height: 500,
-            rowNum: 30,
-            pager: "#jqGridPager"
-    });
-
-    /* disabled polling (using websockets instead)
-    setInterval(function() {
-        grid.trigger("reloadGrid",[{current:true}]);
-    }, 10000);*/
 
     (function(){
         $('body').on('click', '#top-search > a', function(e){
@@ -97,6 +71,61 @@ $(document).ready(function () {
         });
     })();
 });
+
+// Load grid
+var grid = $("#jqGrid").jqGrid({
+    url: '/stations',
+    datatype: "json",
+    colModel: [
+        { label: 'Pole ID', name: 'id', width: 150 },
+        //{ label: 'Street', name: 'street', width: 90 },
+        //{ label: 'City', name: 'city', width: 100 },
+        { label: 'Status', name: 'status', width: 80, sorttype: 'text',  formatter: convertStatus},
+        //{ label: 'Last active', name:'lastHeartbeat', width: 100, formatter:'date', formatoptions: {srcformat: 'U/1000', newformat:'d/m/Y H:i:s'}}
+    ],
+    autowidth: true,
+    shrinkToFit: true,
+    height: 500,
+    rowNum: 30,
+    pager: "#jqGridPager"
+});
+
+$.getJSON( "./stations", function( jsondata ) {
+    json = jsondata;
+    console.log("Loaded location data");
+    // structure which prevents looping in heartbeat updates ;)
+    for (var i = 0; i < json.length; i++) {
+        var pole = json[i];
+        poledata[pole.id] = pole;
+        var curtime = new Date().getTime();
+        if (pole.status != pstatus.DOWN && (pole.lastHeartbeat + DOWNTIMEOUT) > curtime) {
+            // Set timeout
+            poledata[pole.id].timeout = setTimeOutClosure(pole.id);
+        } else {
+            poledata[pole.id].status = pstatus.DOWN;
+
+            // Set pole as offline in grid
+            var rowData = $('#jqGrid').jqGrid('getRowData', pole.id);
+            rowData.status = 0;
+            $('#jqGrid').jqGrid('setRowData', pole.id, rowData);
+        }
+    }
+});
+
+function convertStatus(cellValue)
+{
+    switch(cellValue)
+    {
+        case 0:
+            return "Offline";
+        case 1:
+            return "Idle";
+        case 2:
+            return "Charging";
+        default:
+            return "Unknown";
+    }
+}
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -132,21 +161,6 @@ function initMap() {
                 //console.log(location);
                $("#location").html(location.responseText);
             });
-
-            /*
-            $.ajax({                                            
-                 url: 'location',
-                 data: 'id='+this.url+'&name='+this.name+'',    
-                 success: function(data)          
-                 {   
-                    $("#location").html(data);
-                 },
-                 error: function (xhr, ajaxOptions, thrownError) {
-                    console.log(xhr.status);
-                    console.log(thrownError);
-                 },
-            });
-            */
         });
     }
 };
